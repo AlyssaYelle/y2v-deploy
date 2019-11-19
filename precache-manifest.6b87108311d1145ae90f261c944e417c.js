878e1e825bca67334397b9302e1ba0000165d687
Alyssa <alyssadanijones@gmail.com>
2019-11-18 19:07:43 -0500

    Updates

---
+++ precache-manifest.6b87108311d1145ae90f261c944e417c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b3a6a946cb2ac4b8811ee7c48d30214",
+    "url": "/y2v-deploy/index.html"
+  },
+  {
+    "revision": "27c879cdf83024b3fe63",
+    "url": "/y2v-deploy/static/css/main.17cb1f95.chunk.css"
+  },
+  {
+    "revision": "8d7144b720249b341308",
+    "url": "/y2v-deploy/static/js/2.de8b60fc.chunk.js"
+  },
+  {
+    "revision": "27c879cdf83024b3fe63",
+    "url": "/y2v-deploy/static/js/main.e0d8f066.chunk.js"
+  },
+  {
+    "revision": "448c7bc904b785012eb4",
+    "url": "/y2v-deploy/static/js/runtime-main.38cc0029.js"
+  }
+]);
